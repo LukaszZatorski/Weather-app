e42643c434f37b846b36292e94d2c3da528af0b5
Zatorski <lukasz.zatorskii@gmail.com>
2019-08-05 17:33:27 +0200

    Add geolocalization to LocationSearch

--- src/LocationSearch/index.tsx
+++ src/LocationSearch/index.tsx
@@ -10,6 +10,13 @@ type LatLng = {
   lng: number;
 };
 
+type Position = {
+  coords: {
+    latitude: number;
+    longitude: number;
+  };
+};
+
 type LocationSearchProps = {
   updateLatLng: (latLng: LatLng) => void;
 };
@@ -28,52 +35,91 @@ const LocationSearch = ({ updateLatLng }: LocationSearchProps) => {
       .catch(error => console.error('Error', error));
   };
 
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(setPosition, showError);
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+    }
+  };
+
+  const setPosition = (position: Position) => {
+    const latLng: LatLng = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+    updateLatLng(latLng);
+    setAddress('');
+  };
+
+  const showError = (error: any) => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert('User denied the request for Geolocation.');
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert('Location information is unavailable.');
+        break;
+      case error.TIMEOUT:
+        alert('The request to get user location timed out.');
+        break;
+      case error.UNKNOWN_ERROR:
+        alert('An unknown error occurred.');
+        break;
+    }
+  };
+
   const searchOptions = {
     types: ['(cities)'],
   };
   return (
-    <PlacesAutocomplete
-      highlightFirstSuggestion
-      searchOptions={searchOptions}
-      value={address}
-      onChange={handleChange}
-      onSelect={handleSelect}
-      shouldFetchSuggestions={address.length > 1}
-      googleCallbackName="placesAutocompleteCallback"
-    >
-      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-        <div className="Search-box">
-          <input
-            {...getInputProps({
-              placeholder: 'Enter City...',
-              className: 'location-search-input',
-            })}
-          />
-          {suggestions.length > 0 && (
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map(suggestion => {
-                const className = suggestion.active
-                  ? 'suggestion-item--active'
-                  : 'suggestion-item';
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                    })}
-                  >
-                    <strong>{suggestion.formattedSuggestion.mainText}</strong>{' '}
-                    <small>
-                      {suggestion.formattedSuggestion.secondaryText}
-                    </small>
-                  </div>
-                );
+    <div className="Location-search">
+      <PlacesAutocomplete
+        highlightFirstSuggestion
+        searchOptions={searchOptions}
+        value={address}
+        onChange={handleChange}
+        onSelect={handleSelect}
+        shouldFetchSuggestions={address.length > 1}
+        googleCallbackName="placesAutocompleteCallback"
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div className="Search-box">
+            <input
+              {...getInputProps({
+                placeholder: 'Enter City...',
+                className: 'location-search-input',
               })}
-            </div>
-          )}
-        </div>
-      )}
-    </PlacesAutocomplete>
+            />
+            {suggestions.length > 0 && (
+              <div className="autocomplete-dropdown-container">
+                {loading && <div>Loading...</div>}
+                {suggestions.map(suggestion => {
+                  const className = suggestion.active
+                    ? 'suggestion-item--active'
+                    : 'suggestion-item';
+                  return (
+                    <div
+                      {...getSuggestionItemProps(suggestion, {
+                        className,
+                      })}
+                    >
+                      <strong>{suggestion.formattedSuggestion.mainText}</strong>{' '}
+                      <small>
+                        {suggestion.formattedSuggestion.secondaryText}
+                      </small>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        )}
+      </PlacesAutocomplete>
+      <span className="Geolocation" onClick={getLocation}>
+        <i className="material-icons">my_location</i>
+      </span>
+    </div>
   );
 };
 
